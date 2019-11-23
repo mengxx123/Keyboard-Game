998e26320dba537bfde3070c092cc7d20fd4a161
Andrew Weiss <wandrew8@gmail.com>
2019-11-22 18:13:05 -0800

    user text is being appended properly to page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function playSound(event) {
+function keyDown(event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     key.classList.add('playing');
@@ -6,32 +6,36 @@ function playSound(event) {
     audio.currentTime = 0;
     audio.play();
     randomColor();
-    setTimeout(function() {revertColor(); }, 1000);
-  }
+    setTimeout(function () { revertColor(); }, 1000);
+    createText(event);
+}
 
-  function removeSound(event) {
+function keyUp(event) {
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     key.classList.remove('playing');
-  }
+}
 
-  function randomColor() {
+function createText(event) {
+    let userText = document.createTextNode(event.key);  
+    console.log(event.key)       
+    var node = document.createElement("p");                 
+    node.appendChild(userText);                              
+    document.getElementById("text-display").appendChild(node);
+
+}
+
+function randomColor() {
     let colors = ["#ffa502", "#ff7f50", "#ff6b81", "#ffa502", "#ff6348", "#ff4757", "#7bed9f", "#70a1ff", "#5352ed", "#2ed573", "#1e90ff", "#3742fa", "#a4b0be", "#747d8c", "#ced6e0"];
-  let randColor1 = colors[Math.floor(Math.random()*colors.length)];
-    let randColor2 = colors[Math.floor(Math.random()*colors.length)];
+    let randColor1 = colors[Math.floor(Math.random() * colors.length)];
+    let randColor2 = colors[Math.floor(Math.random() * colors.length)];
     let randomAngle = Math.floor((Math.random() * 360) + 1);
     document.body.style.background = `linear-gradient(${randomAngle}deg, ${randColor1}, ${randColor2})`;
-  };
-  
-  function changeColor() {
-    const colorOne = document.getElementById("color1").value;
-    const colorTwo = document.getElementById("color2").value;
-    const angle = document.getElementById("angle").value;
-    document.body.style.background = `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo})`;
-  };
-  
-  function revertColor() {
-    document.body.style.background = "#444";
-  };
+    document.body.style.transition = "all 2s";
+};
 
-  window.addEventListener("keydown", function (event) { playSound(event) })
-  window.addEventListener("keyup", function (event) { removeSound(event) })
+function revertColor() {
+    document.body.style.background = "#444";
+};
+
+window.addEventListener("keydown", function (event) { keyDown(event) })
+window.addEventListener("keyup", function (event) { keyUp(event) })
