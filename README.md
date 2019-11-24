80460694207bc54eeca8475637b0781d235b40a8
Andrew Weiss <wandrew8@gmail.com>
2019-11-23 17:49:47 -0800

    writing prompts added to game

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Keyboard-Game
+# Rainbow Keyboard
+
+## Overview 
+Rainbow Keyboard is simple desktop typing application that makes typing fun. When the user begins typing
--- app.js
+++ app.js
@@ -7,7 +7,15 @@ function keyDown(event) {
     audio.play();
     randomColor();
     setTimeout(function () { revertColor(); }, 1000);
-    createText(event);
+    if (event.keyCode === 13) {
+        let div = document.getElementById('text-display');
+        div.innerHTML = "";
+        console.log("this is working fine, hmmm...")
+        document.getElementById("writingBoard").innerHTML = "";
+        generateText()
+    } else {
+        createText(event);
+    }
 }
 
 function keyUp(event) {
@@ -25,7 +33,7 @@ function createText(event) {
     if (event.keyCode === 32) {
         node.appendChild(space);
         div.appendChild(node);
-    //This is for the backspace click event
+        //This is for the backspace click event
     } else if (event.keyCode === 8) {
         console.log("the delete button was pressed")
         div.removeChild(div.childNodes[children]);
@@ -33,6 +41,24 @@ function createText(event) {
         node.appendChild(userText);
         div.appendChild(node);
     }
+
+    if (children > 75) {
+        div.innerHTML = "";
+    }
+
+}
+
+function generateText() {
+    let text = ["Typing is fun.", "How fast can you type?", "Pandas are interesting animals.", "Learning how to type properly is an important skill.", "Make sure you aren't looking at your fingers.", "Hey, you're doing really well.", "Gorillas can catch human colds and other illnesses.", "Ostriches can run faster than horses.", "The female lion does ninety percent of the hunting.", "Deer have no gall bladders.", "The bat is the only mammal that can fly.", "A tarantula spider can survive more than two years without food.", "Cows can sleep standing up, but they can only dream lying down.", "The average fox weighs 14 pounds.", "Alligators generally live between 30 and 50 years.", "A single elephant tooth can weigh as much as 9 pounds.", "A housefly hums in the key of F.", "The flamingo can only eat when its head is upside down.", "The smell of a skunk can be detected by a human a mile away."];
+    let randomNum = Math.floor(Math.random() * text.length);
+    let paragraph = document.createElement("h2");
+    let randomText = document.createTextNode(text[randomNum])
+
+    paragraph.appendChild(randomText)
+    document.getElementById("writingBoard").appendChild(paragraph);
+};
+
+function handleScore() {
 
 }
 
@@ -47,7 +73,17 @@ function randomColor() {
 
 function revertColor() {
     document.body.style.background = "#444";
+    document.body.style.transition = "all 2s";
+
 };
 
+function startGame() {
+    document.getElementById("open-modal").classList.add("close-modal-window");
+    document.getElementById("writingBoard").classList.add("writingBoard");
+    generateText();
+}
+
+
+document.getElementById("startButton").onclick = function () { startGame() };
 window.addEventListener("keydown", function (event) { keyDown(event) })
 window.addEventListener("keyup", function (event) { keyUp(event) })
